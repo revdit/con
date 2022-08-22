64e12fb789aedb1cfba3c00c29691067622278b3
revdit <kanjusha1995@gmail.com>
2022-08-22 11:19:17 +0530

    direct home page

--- linking/urls.py
+++ linking/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('masterpage',views.linking_masterpage,name='masterpage'),
-    path('home',views.linking_home,name='home'),
+    path('',views.linking_home,name='home'),
     path('about',views.linking_about,name='about'),
     path('contact',views.linking_contact,name='contact')
 
