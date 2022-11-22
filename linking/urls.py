7d87cbffe6989a02e91985f889743098fff5499e
revdit <kanjusha1995@gmail.com>
2022-11-22 12:47:56 +0530

    changes

--- linking/urls.py
+++ linking/urls.py
@@ -1,10 +1,67 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns=[
     path('masterpage',views.linking_masterpage,name='masterpage'),
     path('',views.linking_home,name='home'),
     path('about',views.linking_about,name='about'),
-    path('contact',views.linking_contact,name='contact')
+    path('contact',views.linking_contact,name='contact'),
+    path('new',views.linking_new,name='new'),
+    path('try',views.linking_try,name='try'),
+    path('baabtra',views.linking_baabtra,name='baabtra'),
+    path('baabtra2',views.linking_baabtra2,name='baabtra2'),
+    path('about1',views.linking_about1,name='about1'),
+    path('blog',views.linking_blog,name='blog'),
+    path('login',views.linking_login,name='login'),
+    path('newpage',views.linking_newpage,name='newpage'),
+    path('browser',views.linking_browser,name='browser'),
+    path('cssrule',views.linking_cssrule,name='cssrule'),
+    path('grid',views.linking_grid,name='grid'),
+    path('gridaline',views.linking_gridaline,name='gridaline'),
+    path('grid1',views.linking_grid1,name='grid1'),
+    path('machinetest',views.linking_machinetest,name='machinetest'),
+    path('facebook',views.linking_facebook,name='facebook'),
+    path('forgot',views.linking_forgot,name='forgot'),
+    path('fpro',views.linking_fpro,name='fpro'),
+    path('fmess',views.linking_fmess,name='fmess'),
+    path('js',views.linking_js,name='js'),
+    path('js1',views.linking_js1,name='js1'),
+    path('fact',views.linking_fact,name='fact'),
+    path('pali',views.linking_pali,name='pali'),
+    path('funcion',views.linking_funcion,name='funcion'),
+    path('sumpr',views.linking_sumpr,name='sumpr'),
+    path('eveod',views.linking_eveod,name='eveod'),
+    path('sumdi',views.linking_sumdi,name='sumdi'),
+    path('fun1',views.linking_fun1,name='fun1'),
+    path('countele',views.linking_countele,name='countele'),
+    path('large',views.linking_large,name='large'),
+    path('fib',views.linking_fib,name='fib'),
+    path('dom',views.linking_dom,name='dom'),
+    path('largesec',views.linking_largesec,name='largesec'),
+    path('dom2',views.linking_dom2,name='dom2'),
+    path('dom3',views.linking_dom3,name='dom3'),
+    path('pattern',views.linking_pattern,name='pattern'),
+    path('array',views.linking_array,name='array'),
+    path('todo',views.linking_todo,name='todo'),
+    path('dom4',views.linking_dom4,name='dom4'),
+    path('dom5',views.linking_dom5,name='dom5'),
+    path('jq',views.linking_jq,name='jq'),
+    path('jq1',views.linking_jq1,name='jq1'),
+    path('jq2',views.linking_jq2,name='jq2'),
+    path('jqvalidation',views.linking_jqvalidation,name='jqvalidation'),
+    path('mttodo',views.linking_mttodo,name='mttodo'),
+     path('newtodo',views.linking_newtodo,name='newtodo'),
+     path('calc',views.linking_calc,name='calc'),
+
+
+
+
+    
+
+
+
+    
+    
 
 ]
--- linking/views.py
+++ linking/views.py
@@ -12,3 +12,141 @@ def linking_about(request):
 
 def linking_contact(request):
     return render(request,'linking/contact.html')
+
+def linking_new(request):
+    return render(request,'linking/new.html')
+
+def linking_try(request):
+    return render(request,'linking/try.html')
+
+def linking_baabtra(request):
+    return render(request,'linking/baabtra.html')
+
+def linking_baabtra2(request):
+    return render(request,'linking/baabtra2.html')
+ 
+def linking_about1(request):
+    return render(request,'linking/about1.html')
+     
+def linking_blog(request):
+    return render(request,'linking/blog.html') 
+    
+def linking_login(request):
+    return render(request,'linking/login.html')
+
+def linking_newpage(request):
+    return render(request,'linking/newpage.html')
+    
+def linking_browser(request):
+    return render(request,'linking/browser.html')
+
+def linking_cssrule(request):
+    return render(request,'linking/cssrule.html')
+
+def linking_grid(request):
+    return render(request,'linking/grid.html')
+
+def linking_gridaline(request):
+    return render(request,'linking/gridaline.html')
+
+def linking_grid1(request):
+    return render(request,'linking/grid1.html')
+
+def linking_machinetest(request):    
+    return render(request,'linking/machinetest.html')
+
+def linking_facebook(request):
+    return render(request,'linking/facebook.html')
+
+def linking_forgot(request):
+    return render(request,'linking/forgot.html')
+
+def linking_fpro(request):
+    return render(request,'linking/fpro.html')
+
+def linking_fmess(request):
+    return render(request,'linking/fmess.html') 
+def linking_js(request):
+    return render(request,'linking/js.html')
+    
+def linking_js1(request):
+    return render(request,'linking/js1.html')
+
+def linking_fact(request):
+    return render(request,'linking/fact.html')
+
+def linking_pali(request):
+    return render(request,'linking/pali.html')
+
+def linking_funcion(request):
+    return render(request,'linking/funcion.html')
+    
+def linking_sumpr(request):
+    return render(request,'linking/sumpr.html')
+
+def linking_eveod(request):
+    return render(request,'linking/eveod.html')
+
+def linking_sumdi(request):
+    return render(request,'linking/sumdi.html')
+
+def linking_fun1(request):
+    return render(request,'linking/fun1.html')
+
+def linking_countele(request):
+    return render(request,'linking/countele.html')
+
+def linking_large(request):
+    return render(request,'linking/large.html')
+
+def linking_fib(request):
+    return render(request,'linking/fib.html')
+
+def linking_fib(request):
+    return render(request,'linking/fib.html')
+
+def linking_dom(request):
+    return render(request,'linking/dom.html')
+def linking_largesec(request):
+    return render(request,'linking/largesec.html')
+
+def linking_dom2(request):
+    return render(request,'linking/dom2.html')
+
+def linking_dom3(request):
+    return render(request,'linking/dom3.html')
+
+def linking_pattern(request):
+    return render(request,'linking/pattern.html')
+
+def linking_array(request):
+    return render(request,'linking/array.html')
+
+def linking_todo(request):
+    return render(request,'linking/todo.html')
+def linking_dom4(request):
+    return render(request,'linking/dom4.html')
+
+def linking_dom5(request):
+    return render(request,'linking/dom5.html')
+
+def linking_jq(request):
+    return render(request,'linking/jq.html')
+
+def linking_jq1(request):
+    return render(request,'linking/jq1.html')
+
+def linking_jq2(request):
+    return render(request,'linking/jq2.html')
+
+def linking_jqvalidation(request):
+    return render(request,'linking/jqvalidation.html')
+
+def linking_mttodo(request):
+    return render(request,'linking/mttodo.html')
+
+def linking_newtodo(request):
+    return render(request,'linking/newtodo.html')
+
+def linking_calc(request):
+    return render(request,'linking/calc.html')
